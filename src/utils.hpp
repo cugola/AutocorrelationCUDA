84c44f5474fe5f3d05576f002c3b98527f10c9c8
Gianpaolo Cugola <gianpaolo.cugola@polimi.it>
2024-01-20 19:17:53 +0100

    Fixed bug in calculating max_lag. Reformatted file
    - Fixed a bug in the way the max_lag was calculated by the generate_taus function,
      which resulted in exporting less lags than those actually computed by the correlator
    - Reformatted entire file

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,103 +1,95 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <stdexcept>
 #include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 namespace utils {
 
-    template <typename T> 
-    T convert_to(const std::string &cell) {
-        std::istringstream ss(cell);
-        T value;
+template <typename T>
+T convert_to(const std::string &cell) {
+  std::istringstream ss(cell);
+  T value;
 
-        ss >> value;
+  ss >> value;
 
-        if (ss.fail()){
-            throw std::runtime_error("ERROR: Could not convert content to type");
-        }
+  if(ss.fail()) {
+    throw std::runtime_error("ERROR: Could not convert content to type");
+  }
 
-        return value;
-    }
-
-    template <typename T>
-    std::vector<T> parse_csv_line(const std::string &line){
-        std::vector<T> result;
-        std::stringstream lineStream(line);
-        std::string cell;
-
-        while(std::getline(lineStream, cell, ',')){
-            result.push_back(convert_to<T>(cell));
-        }
-
-        return result;
-    }
-
-
-    template <typename T>
-    std::vector<T> parse_csv(const std::string &filepath) {
-
-        std::vector<T> result;
-        std::ifstream file(filepath);
-
-        if(!file.is_open()) throw std::runtime_error("ERROR: Could not open file" + filepath);
-
-        //Skip header line
-        std::string line;
-        std::getline(file, line);
-
-        //Read data into result array
-        while(std::getline(file, line)) {
-            std::vector<T> lineData = parse_csv_line<T>(line);
-            result.insert(result.end(), lineData.begin(), lineData.end());
-        }
-
-        file.close();
-        return result;
-
-    }
-
-
-    template<typename T>
-    std::vector<T> slice(std::vector<T> const &v, int m, int n)
-    {
-        auto first = v.cbegin() + m;
-        auto last = v.cbegin() + n + 1;
-    
-        std::vector<T> vec(first, last);
-        return vec;
-    }
-
-
-    std::vector<uint16_t> generate_taus(const size_t full_time_len, const size_t bin_size, const size_t num_bins, const size_t m = 2) {
-        std::vector<uint16_t> taus;
-        size_t max_lag = std::pow((double)m, num_bins - 1) * (bin_size/2) + bin_size;        
-
-        for (size_t i = 0; i < bin_size; ++i){
-            if (i > full_time_len){
-                    return taus;
-            }
-            taus.push_back(i);
-        }
-
-        for (size_t i = 1; i < num_bins; ++i) {
-            for(size_t j = (bin_size/2); j < bin_size; ++j){               
-
-                size_t p = std::pow((double)m,i);
-                size_t tau = j * p;
-                if (tau > max_lag || tau > full_time_len - p){
-
-
-                    return taus;
-                }
-                taus.push_back(tau);
-            }
-        }
-
-        return taus;
-    }
+  return value;
 }
+
+template <typename T>
+std::vector<T> parse_csv_line(const std::string &line) {
+  std::vector<T> result;
+  std::stringstream lineStream(line);
+  std::string cell;
+
+  while(std::getline(lineStream, cell, ',')) {
+    result.push_back(convert_to<T>(cell));
+  }
+
+  return result;
+}
+
+template <typename T>
+std::vector<T> parse_csv(const std::string &filepath) {
+  std::vector<T> result;
+  std::ifstream file(filepath);
+
+  if(!file.is_open()) throw std::runtime_error("ERROR: Could not open file" + filepath);
+
+  // Skip header line
+  std::string line;
+  std::getline(file, line);
+
+  // Read data into result array
+  while(std::getline(file, line)) {
+    std::vector<T> lineData = parse_csv_line<T>(line);
+    result.insert(result.end(), lineData.begin(), lineData.end());
+  }
+
+  file.close();
+  return result;
+}
+
+template <typename T>
+std::vector<T> slice(std::vector<T> const &v, int m, int n) {
+  auto first = v.cbegin() + m;
+  auto last = v.cbegin() + n + 1;
+
+  std::vector<T> vec(first, last);
+  return vec;
+}
+
+std::vector<uint16_t> generate_taus(const size_t full_time_len, const size_t bin_size, const size_t num_bins, const size_t m = 2) {
+  std::vector<uint16_t> taus;
+  // size_t max_lag = std::pow((double)m, num_bins - 1) * (bin_size/2) + bin_size;
+  size_t max_lag = std::pow((double)m, num_bins - 1) * (bin_size - 1);
+
+  for(size_t i = 0; i < bin_size; ++i) {
+    if(i > full_time_len) {
+      return taus;
+    }
+    taus.push_back(i);
+  }
+
+  for(size_t i = 1; i < num_bins; ++i) {
+    for(size_t j = (bin_size / 2); j < bin_size; ++j) {
+      size_t p = std::pow((double)m, i);
+      size_t tau = j * p;
+      if(tau > max_lag || tau > full_time_len - p) {
+        return taus;
+      }
+      taus.push_back(tau);
+    }
+  }
+
+  return taus;
+}
+}  // namespace utils
